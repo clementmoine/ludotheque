clementmoine/tusmo

Chore: Create monorepo project

packages/client/src/App.tsx
@@ -1,0 +1,55 @@
+import { useState } from 'react';
+
+import { DemoResponse } from '@tusmo/server';
+
+import logo from './logo.svg';
+
+import './App.scss';
+
+function App() {
+  /**
+   * It fetches the data from the server and then prints it to the console
+   */
+  const handleClick = () => {
+    fetch('http://localhost:8000/demo', {})
+      .then((response) => response.json())
+      .then((data: DemoResponse) => console.log(data));
+  };
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button onClick={handleClick}>Ping server</button>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

clementmoine/ludotheque

packages/client/src/App.tsx
@@ -1,0 +1,55 @@
+import { useState } from 'react';
+
+import { DemoResponse } from '@ludotheque/server';
+
+import logo from './logo.svg';
+
+import './App.scss';
+
+function App() {
+  /**
+   * It fetches the data from the server and then prints it to the console
+   */
+  const handleClick = () => {
+    fetch('http://localhost:8000/demo', {})
+      .then((response) => response.json())
+      .then((data: DemoResponse) => console.log(data));
+  };
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button onClick={handleClick}>Ping server</button>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;